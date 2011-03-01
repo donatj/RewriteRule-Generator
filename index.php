7e6587b204e3de46d465ff07e78a36bddeac6014
Jesse Donat <donatj@gmail.com>
2011-02-28 18:25:50 -0600

    License and author added, spacing tolerance added, slight restructuring.

--- index.php
+++ index.php
@@ -1,44 +1,48 @@
 <?php
 
-$data = "http://www.test.com/test.html	http://www.test.com/spiders.html
-http://www.test.com/faq.html?faq=13	http://www.test.com/faqs.html?id=10
-text/faq.html?faq=20	helpdesk/kb.php";
+/**
+* RewriteRule Generator
+* 
+* @license MIT
+* @author Jesse G. Donat <donatj@gmail.com> http://donatstudios.com
+* 
+*/
 
-$lines = explode(PHP_EOL, $_POST['tabbed_rewrites'] );
+if( $_POST ) {
+	$_POST['tabbed_rewrites'] = preg_replace('/(\t| )+/', '	', $_POST['tabbed_rewrites']); // Spacing Cleanup
+	$lines = explode(PHP_EOL, $_POST['tabbed_rewrites'] );
 
-$str = '';
-if( strlen(trim($_POST['tabbed_rewrites'])) ) {
-	foreach( $lines as $line ) {
-		$ab = explode("	", $line);
-		
-		$ab0p = parse_url( trim($ab[0]) );
-		$ab1p = parse_url( trim($ab[1]) );
-		
-		if( $_POST['desc_comments'] ) { $str .= PHP_EOL . '#   ---   ' . $ab[0] . ' => ' . $ab[1] . PHP_EOL; }
-		
-		$ab0pqs = explode('&', $ab0p['query']);
-		foreach( $ab0pqs as $qs ) {
-			if( strlen( $qs ) > 0 ) {
-				$str .= 'RewriteCond %{QUERY_STRING} (^|&)'. quotemeta($qs) .'($|&)';
-				$str .= PHP_EOL;
+	$str = '';
+	if( strlen(trim($_POST['tabbed_rewrites'])) ) {
+		foreach( $lines as $line ) {
+			$ab = explode("	", $line);
+
+			$ab0p = parse_url( trim($ab[0]) );
+			$ab1p = parse_url( trim($ab[1]) );
+
+			if( $_POST['desc_comments'] ) { $str .= PHP_EOL . '#   ---   ' . $ab[0] . ' => ' . $ab[1] . PHP_EOL; }
+
+			$ab0pqs = explode('&', $ab0p['query']);
+			foreach( $ab0pqs as $qs ) {
+				if( strlen( $qs ) > 0 ) {
+					$str .= 'RewriteCond %{QUERY_STRING} (^|&)'. quotemeta($qs) .'($|&)';
+					$str .= PHP_EOL;
+				}
 			}
-		}
-		
-		$str .= 'RewriteRule ^'.quotemeta(ltrim($ab0p['path'],'/')).'$ '.ltrim( $ab1p['path'], '/' ).'?'.$ab1p['query'] . ( $_POST['type'] == 'Rewrite' ? '&%{QUERY_STRING}':' [L,R=301]' );
-		$str .= PHP_EOL;
-		
-	}
-	
-	$data = $_POST['tabbed_rewrites'];
-}
 
-if( !$_POST ) {
+			$str .= 'RewriteRule ^'.quotemeta(ltrim($ab0p['path'],'/')).'$ '.ltrim( $ab1p['path'], '/' ).'?'.$ab1p['query'] . ( $_POST['type'] == 'Rewrite' ? '&%{QUERY_STRING}':' [L,R=301]' );
+			$str .= PHP_EOL;
+
+		}
+	}
+}else{
 	$_POST['desc_comments'] = 1;
+	$_POST['tabbed_rewrites'] = "http://www.test.com/test.html	http://www.test.com/spiders.html" . PHP_EOL . "http://www.test.com/faq.html?faq=13	http://www.test.com/faqs.html?id=10" . PHP_EOL . "text/faq.html?faq=20	helpdesk/kb.php";
 }
 
 ?>
 <form method="post">
-	<textarea cols="100" rows="20" name="tabbed_rewrites"><?php echo htmlentities( $data ) ?></textarea><br />
+	<textarea cols="100" rows="20" name="tabbed_rewrites"><?php echo htmlentities( $_POST['tabbed_rewrites'] ) ?></textarea><br />
 	<select name="type">
 		<option>301</option>
 		<option<?php echo $_POST['type'] == 'Rewrite' ? ' selected="selected"' : '' ?>>Rewrite</option>
