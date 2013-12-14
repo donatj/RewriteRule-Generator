823fc4249feed76d4bad9bb412f8e01fdd6d9221
Jesse Donat <donatj@gmail.com>
2013-12-14 16:39:47 -0600

    Changed variable names for readability. WTF was I thinking with those names

--- index.php
+++ index.php
@@ -8,47 +8,48 @@
  *
  */
 
-if( $_POST ) {
+$output = '';
+
+if( $_POST['tabbed_rewrites'] ) {
 	$_POST['tabbed_rewrites'] = preg_replace('/(\t| )+/', '	', $_POST['tabbed_rewrites']); // Spacing Cleanup
 	$lines                    = explode(PHP_EOL, $_POST['tabbed_rewrites']);
 
-	$str = '';
 	if( strlen(trim($_POST['tabbed_rewrites'])) ) {
 		foreach( $lines as $line ) {
 			$line = trim($line);
 			if( $line == '' ) continue;
-			$ab = explode("	", $line);
+			$explodedLine = explode("	", $line);
 
-			if( count($ab) != 2 ) {
-				$str .= PHP_EOL . '# MALFORMED LINE SKIPPED: ' . $line . PHP_EOL;
+			if( count($explodedLine) != 2 ) {
+				$output .= PHP_EOL . '# MALFORMED LINE SKIPPED: ' . $line . PHP_EOL;
 				continue;
 			}
 
-			$ab0p = parse_url(trim($ab[0]));
-			$ab1p = parse_url(trim($ab[1]));
+			$parsedFrom = parse_url(trim($explodedLine[0]));
+			$parsedTo   = parse_url(trim($explodedLine[1]));
 
 			if( $_POST['desc_comments'] ) {
-				$str .= PHP_EOL . '# ' . $_POST['type'] . ' --- ' . $ab[0] . ' => ' . $ab[1] . PHP_EOL;
+				$output .= PHP_EOL . '# ' . $_POST['type'] . ' --- ' . $explodedLine[0] . ' => ' . $explodedLine[1] . PHP_EOL;
 			}
 
-			if( $ab0p['host'] != $ab1p['host'] ) {
-				$str .= 'RewriteCond %{HTTP_HOST} ^' . quotemeta($ab0p['host']) . '$';
-				$str .= PHP_EOL;
-				$prefix = $ab1p['scheme'] . '://' . $ab1p['host'] . '/';
+			if( $parsedFrom['host'] != $parsedTo['host'] ) {
+				$output .= 'RewriteCond %{HTTP_HOST} ^' . quotemeta($parsedFrom['host']) . '$';
+				$output .= PHP_EOL;
+				$prefix = $parsedTo['scheme'] . '://' . $parsedTo['host'] . '/';
 			} else {
 				$prefix = '/';
 			}
 
-			$ab0pqs = explode('&', $ab0p['query']);
-			foreach( $ab0pqs as $qs ) {
+			$explodedQuery = explode('&', $parsedFrom['query']);
+			foreach( $explodedQuery as $qs ) {
 				if( strlen($qs) > 0 ) {
-					$str .= 'RewriteCond %{QUERY_STRING} (^|&)' . quotemeta($qs) . '($|&)';
-					$str .= PHP_EOL;
+					$output .= 'RewriteCond %{QUERY_STRING} (^|&)' . quotemeta($qs) . '($|&)';
+					$output .= PHP_EOL;
 				}
 			}
 
-			$str .= 'RewriteRule ^' . quotemeta(ltrim($ab0p['path'], '/')) . '$ ' . $prefix . ltrim($ab1p['path'], '/') . '?' . $ab1p['query'] . ($_POST['type'] == 'Rewrite' ? '&%{QUERY_STRING}' : ' [L,R=301]');
-			$str .= PHP_EOL;
+			$output .= 'RewriteRule ^' . quotemeta(ltrim($parsedFrom['path'], '/')) . '$ ' . $prefix . ltrim($parsedTo['path'], '/') . '?' . $parsedTo['query'] . ($_POST['type'] == 'Rewrite' ? '&%{QUERY_STRING}' : ' [L,R=301]');
+			$output .= PHP_EOL;
 		}
 	}
 } else {
@@ -65,6 +66,6 @@ if( $_POST ) {
 	</select>
 	<label><input type="checkbox" name="desc_comments" value="1"<?php echo $_POST['desc_comments'] ? ' checked="checked"' : '' ?>>Comments</label>
 	<br />
-	<textarea cols="100" rows="20" readonly="readonly" style="width: 100%; height: 265px;"><?php echo htmlentities($str) ?></textarea><br />
+	<textarea cols="100" rows="20" readonly="readonly" style="width: 100%; height: 265px;"><?php echo htmlentities($output) ?></textarea><br />
 	<center><input type="submit" /></center>
 </form>
