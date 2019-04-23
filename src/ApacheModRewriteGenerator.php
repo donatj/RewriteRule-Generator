34567894f9d3c440c3b2017e6db66786ec92a8a0
Jesse Donat <donatj@gmail.com>
2019-04-23 15:41:43 -0500

    Cleaner array key handling

--- src/ApacheModRewriteGenerator.php
+++ src/ApacheModRewriteGenerator.php
@@ -15,29 +15,31 @@ class ApacheModRewriteGenerator implements GeneratorInterface {
 		$parsedFrom = parse_url($from);
 		$parsedTo   = parse_url($to);
 
-		$parsedFrom['host']  = $parsedFrom['host'] ?? '';
-		$parsedTo['host']    = $parsedTo['host'] ?? '';
+		$toScheme = $parsedTo['scheme'] ?? '';
 
-		$parsedFrom['query'] = $parsedFrom['query'] ?? '';
-		$parsedTo['query']   = $parsedTo['query'] ?? '';
+		$fromHost = $parsedFrom['host'] ?? '';
+		$toHost   = $parsedTo['host'] ?? '';
 
-		$parsedFrom['path'] = $parsedFrom['path'] ?? '';
-		$parsedTo['path']   = $parsedTo['path'] ?? '';
+		$fromQuery = $parsedFrom['query'] ?? '';
+		$toQuery   = $parsedTo['query'] ?? '';
+
+		$fromPath = $parsedFrom['path'] ?? '';
+		$toPath   = $parsedTo['path'] ?? '';
 
 		$output = '';
 
-		if( !$parsedFrom['host'] && $parsedTo['host'] ) {
+		if( !$fromHost && $toHost ) {
 			throw new AmbiguousRelativeHostException('Unclear relative host. When the "FROM" URI specifies a HOST the "TO" MUST specify a HOST as well.');
 		}
-		if( $parsedTo['host'] && $parsedFrom['host'] !== $parsedTo['host'] ) {
-			$output .= 'RewriteCond %{HTTP_HOST} ^' . preg_quote($parsedFrom['host']) . '$';
+		if( $toHost && $fromHost !== $toHost ) {
+			$output .= 'RewriteCond %{HTTP_HOST} ^' . preg_quote($fromHost) . '$';
 			$output .= "\n";
-			$prefix = $parsedTo['scheme'] . '://' . $parsedTo['host'] . '/';
+			$prefix = $toScheme . '://' . $toHost . '/';
 		} else {
 			$prefix = '/';
 		}
 
-		$explodedQuery = explode('&', $parsedFrom['query']);
+		$explodedQuery = explode('&', $fromQuery);
 		foreach( $explodedQuery as $qs ) {
 			if( $qs !== '' ) {
 				$output .= 'RewriteCond %{QUERY_STRING} (^|&)' . preg_quote($qs) . '($|&)';
@@ -45,7 +47,7 @@ class ApacheModRewriteGenerator implements GeneratorInterface {
 			}
 		}
 
-		$output .= 'RewriteRule ^' . preg_quote(ltrim($parsedFrom['path'], '/')) . '$ ' . $prefix . ltrim($parsedTo['path'], '/') . '?' . $parsedTo['query'];
+		$output .= 'RewriteRule ^' . preg_quote(ltrim($fromPath, '/')) . '$ ' . $prefix . ltrim($toPath, '/') . '?' . $toQuery;
 
 		switch( $type ) {
 			case RewriteTypes::SERVER_REWRITE:
